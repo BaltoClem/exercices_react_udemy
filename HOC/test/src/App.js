882c73fc0db88ec402fbd71d9a44f51587e3e751
Clement <dumasclementjulien@yahoo.fr>
2023-09-12 00:09:30 +0200

    Exercices HOC en cours

---
+++ HOC/test/src/App.js
@@ -0,0 +1,26 @@
+// Les Concepts de React Suite - 006 Les Composants d'Ordre Supérieur - HOC 14:36
+import React, { Component } from "react";
+import Vegeta from "./Vegeta";
+import Goku from "./Goku";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container text-center">
+          <h1>Goku VS Vegeta</h1>
+          <hr/>
+
+          <div className="row">
+
+              <Vegeta name="Vegeta" />
+              <Goku name="Goku" />
+
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
