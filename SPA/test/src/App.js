c3e0b1d5dd298b10ea16438c688807ec797a7b2d
Clement <dumasclementjulien@yahoo.fr>
2023-10-04 16:41:05 +0200

    Exercices Redirections terminés

--- SPA/test/src/App.js
+++ SPA/test/src/App.js
@@ -1,27 +1,47 @@
 // Les Concepts de React Suite - 009 BrowserRoute, Route, Switch, Nav, NavLink, Strict et Exact - 17:24
-import React from 'react';
+import React, { Component } from 'react';
 import Docs from './components/Docs';
 import Tutorials from './components/Tutorials';
 import Community from './components/Community';
 import Menu from './components/Menu';
 import ErrorPage from './components/ErrorPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
-function App() {
+class App extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     underConst: {
+      Docs: false,
+      Tutorials: true,
+      Community: false
+     }
+  }
+}
+
+  render(){
   return (
     <BrowserRouter>
       <Menu />
         <Routes>
           <Route exact path='/' element={<Docs />} />
           <Route path='/tutorials' element={<Tutorials />} />
+
+          {/* <Route path='/tutorials' render={() => (
+            this.state.underConst.Tutorials ? (<Redirect to="/"/>) : (<Tutorials />)
+          )} /> */}
+
+
           <Route path='/community' element={<Community />} />
           <Route path='*' element={<ErrorPage />} />
         </Routes>
     </BrowserRouter>
   );
+}
 }
 
 export default App;
